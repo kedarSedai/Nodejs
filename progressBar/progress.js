b71a6c4aa851d32a0f415bb73e429250d6c9c67e
kedar sedai <kedar.sedai05@gmail.com>
2020-07-29 08:13:36 +0545

    Reding from console

--- progressBar/progress.js
+++ progressBar/progress.js
@@ -1,10 +1,11 @@
 const progress = require('progress');
+const chalk = require('chalk');
 
 var bar = new progress(':bar', 10 );
 var timer = setInterval(function () {
   bar.tick();
   if (bar.complete) {
-    console.log('\ncomplete\n');
+    console.log(chalk.yellow('Completed'))
     clearInterval(timer);
   }
 }, 100);
