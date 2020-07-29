85cdc4405077e41c98b37e9b90ac571db5c2ec05
kedar sedai <kedar.sedai05@gmail.com>
2020-07-29 08:07:36 +0545

    Added progressbar and chalk

---
+++ progressBar/progress.js
@@ -0,0 +1,10 @@
+const progress = require('progress');
+
+var bar = new progress(':bar', 10 );
+var timer = setInterval(function () {
+  bar.tick();
+  if (bar.complete) {
+    console.log('\ncomplete\n');
+    clearInterval(timer);
+  }
+}, 100);
