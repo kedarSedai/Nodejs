de351fae8baae363056c419c2141d3d00d42a059
kedar sedai <kedar.sedai05@gmail.com>
2020-07-29 07:55:57 +0545

    Counting Elements

---
+++ countingElements/counting.js
@@ -0,0 +1,11 @@
+const x = 1;
+const y = 2;
+//counting numbers
+console.count('Number is ' +x);
+console.count(`Number is ${y}`);
+
+//counting fruits 
+const oranges = ['oranges', 'orange', 'oranges'];
+const apple = ['apple', 'apples'];
+
+oranges.forEach(fruit => console.count(fruit))
